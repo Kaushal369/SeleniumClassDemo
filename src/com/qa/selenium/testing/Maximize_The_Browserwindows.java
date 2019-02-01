97070413f96ae3d9579eef7ce39efa1c6ddc0069
kaushal369@gmail.com <kaushal369@gmail.com>
2019-02-01 17:28:59 +0530

    add try block in MWclass

--- src/com/qa/selenium/testing/Maximize_The_Browserwindows.java
+++ src/com/qa/selenium/testing/Maximize_The_Browserwindows.java
@@ -1,10 +1,42 @@
 package com.qa.selenium.testing;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Maximize_The_Browserwindows {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			WebDriver driver = new FirefoxDriver();
 
+			driver.get("http://google.com/");
+			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+			driver.findElement(By.name("q")).sendKeys("selenium");
+
+			List<WebElement> elements = driver.findElements(By.xpath(".//ul[@role='listbox']/li"));
+
+			System.out.println(elements.size());
+
+			for (WebElement element : elements) {
+				if (element.getText().equalsIgnoreCase("selenium download")) {
+					element.click();
+					break;
+				}
+			}
+
+			driver.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
